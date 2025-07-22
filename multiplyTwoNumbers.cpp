0408c786a89013e79da55eb3f8de911ee8040648
sila-electro <Ssilasay1053@gmail.com>
2025-07-23 00:27:10 +0300

    Add files via upload

---
+++ multiplyTwoNumbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int multiplyTwoNumbers(int x, int y){
+	return x * y;
+}
+
+int main() {
+	int result = multiplyTwoNumbers(4, 5);
+	cout << "Result: " << result << endl;
+	return 0;
+}
+
