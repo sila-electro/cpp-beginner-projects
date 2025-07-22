6acfd03de5d4057be923bd90818beb30d75ffc24
sila-electro <Ssilasay1053@gmail.com>
2025-07-22 14:34:34 +0300

    Add files via upload

---
+++ array-analysis.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int SIZE = 5;
+    int numbers[SIZE];
+
+    cout << "5 sayı girin:\n";
+    for (int i = 0; i < SIZE; i++) {
+        cin >> numbers[i];
+    }
+
+    int max = numbers[0];
+    int min = numbers[0];
+    int sum = 0;
+
+    for (int i = 0; i < SIZE; i++) {
+        if (numbers[i] > max) max = numbers[i];
+        if (numbers[i] < min) min = numbers[i];
+        sum += numbers[i];
+    }
+
+    double average = static_cast<double>(sum) / SIZE;
+
+    cout << "En büyük sayı: " << max << endl;
+    cout << "En küçük sayı: " << min << endl;
+    cout << "Ortalama: " << average << endl;
+
+    return 0;
+}
+
